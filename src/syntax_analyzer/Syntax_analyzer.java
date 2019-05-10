1d6095c0531766b050d32d99d3281552ddf4b5c7
choiyoungjae <dudwo1344@gmail.com>
2019-05-10 23:22:46 +0900

    add syntax analyzer constuctor
    add syntax analyzer constructor

--- src/syntax_analyzer/Syntax_analyzer.java
+++ src/syntax_analyzer/Syntax_analyzer.java
@@ -1,10 +1,19 @@
 package syntax_analyzer;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+
+import org.apache.poi.ss.formula.functions.T;
 
 public class Syntax_analyzer {
 	//STR-table
-	public HashMap<Integer, HashMap<String, String>> slrTable;
+	private HashMap<Integer, HashMap<String, String>> slrTable;
+	private ArrayList<String> symbolTable;
+	private  ArrayList<T> production;
 	
-
+	Syntax_analyzer(HashMap<Integer, HashMap<String, String>> slrTable,ArrayList<String> symbolTable, ArrayList<T> production){
+		this.slrTable = slrTable;
+		this.symbolTable = symbolTable;
+		this.production = production;
+	}
 }
